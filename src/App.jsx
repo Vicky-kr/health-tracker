9e4a1738601f594af5b70242f5bc03a878bc3df6
VAIBHAV SACHDEVA <83213763+vaibhav21git@users.noreply.github.com>
2021-09-23 19:41:43 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import Template2 from './Template2';
+//  import Template3 from './Template3';
+//  import Template4 from './Template3';
+import Template2 from './Template2';
+
+
+const App = () =>
+{
+    return(
+     <Template2/>
+    )
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import App from "./app";
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import App  from "./App"
+  
 
-ReactDOM.render(
+
+  ReactDOM.render
+  (
     <>
-        <App/>
-    </>,
-    document.getElementById('root')
-)
+      <App/>
+      </>
+    ,document.getElementById('root')
+  );
+    
