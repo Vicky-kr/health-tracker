0c595de6895d825bc10da461da4781d0bd2b0815
Vicky <kr.vicky@outlook.com>
2021-09-24 12:40:19 +0530

    Modified with folder structure

---
+++ src/Pages/LoginPage/Template4.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import './Template4.css'
+import Rightpart from "../../Rightpart";
+import background from '../../bgimg.png';
+import {Navbar} from "../../Navbar/Navbar";
+
+
+const Template4 = () => {
+    return (
+        <>
+            <Navbar/>
+            <div className="mainc3">
+                <div className="lp3" style={{backgroundImage: `url(${background})`}}>
+
+
+                    <h1 className="heading">LOGIN</h1>
+
+                    <label for="emailid">@</label>
+                    <input type="text" id="emailid" name="emailid" placeholder="Enter your email id"></input>
+                    <label for="password">PP</label>
+                    <input type="text" id="password" name="password" placeholder="Enter your password"></input>
+
+                    <button className="subleft btnn ">forgot password ?</button>
+                    <button className="subleft btnn">login</button>
+                    <div className="account subleft ">
+                        <span> Don't have an account ?</span>
+                        <button className="subleft btnn">create one</button>
+                    </div>
+
+                </div>
+                <div className="rp3">
+                    <Rightpart/>
+                </div>
+            </div>
+
+        </>
+    )
+};
+
+export default Template4;
