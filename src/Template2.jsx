c0785a2e19d1c0cb6c701a3f760b532e4824a744
VAIBHAV SACHDEVA <83213763+vaibhav21git@users.noreply.github.com>
2021-09-27 22:07:14 +0530

    Update Template2.jsx

--- src/Template2.jsx
+++ src/Template2.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import "./csst/csst2.css";
 import Rightpart from "./Rightpart";
 import {Navbar} from "./Navbar/Navbar";
+import { useHistory } from "react-router-dom";
 
 const Template2 = () => {
+
+    const  history  = useHistory();
     return (
         <div>
            <Navbar/>
             <div className="maincontent">
                 <div className="leftarea">
-                    <h1 className="leftareachild">VIRTUAL HEALTH CARE FOR YOU</h1>
+                    <h1 className="leftareachildh">VIRTUAL HEALTH CARE FOR YOU</h1>
                     <p className="leftareachild">Lorem ipsum dolor sit, amet consectet Quam commodi, dolorem assumenda
                         iusto expedita corporis ipsum aperiape optio debitis, totam alias iusto porro quia provident,
                         iure placeat architecto cum ipsam autem distinctio iste? Aliquam ut accusantium perferendis
                         non.</p>
-                    <button className="leftareabutton leftareachild">CONSULT TODAY</button>
+                    <button  onClick = {()=> history.push('/signuppage')} className="leftareabutton leftareachild">CONSULT TODAY</button>
                 </div>
                 <Rightpart/>
             </div>
@@ -23,5 +26,7 @@ const Template2 = () => {
 };
 
 export default Template2;
+
+
 
 
