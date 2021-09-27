7e17274a8cf6138388fa522395ea2bb6fbde4b52
VAIBHAV SACHDEVA <83213763+vaibhav21git@users.noreply.github.com>
2021-09-27 22:09:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from "react";
 import Template2 from './Template2';
+import {Route , Switch} from "react-router-dom"
 import Authenticate from "./Pages/FaceAuthenticationPage/Authenticate";
 import UserDuotone from "./Pages/FaceAuthenticationPage/UserDuotone";
 import Dashboard from "./Pages/Dashboard/Dashboard";
+import Template3 from "./Pages/SignUpPage/Template3";
+import Template4 from "./Pages/LoginPage/Template4";
+import Template5 from "./Pages/FillInformationPage/Template5";
+import DashboardBottom from "./Pages/Dashboard/DashboardBottom";
+import Error from "./Error";
+
+
+
+
+
 
 
 
 const App = () =>
 {
     return(
-    //  <Authenticate/>
-    //   <UserDuotone/>
-    <Dashboard/>
-    )
+        <>
+        <Switch>
+            <Route  exact path = '/' component = {Template2}/>
+            <Route exact path = '/loginpage' component = {Template4}/>
+            <Route exact path = '/signuppage' component = {Template3}/>
+            <Route exact path = '/fillformpage' component = {Template5}/>
+            <Route exact path = './dashboard' component = {Dashboard}/>
+            <Route exact path = './userduotone' component = {UserDuotone}/>
+            <Route exact path = './authenticate' component = {Authenticate}/>
+            <Route exact path = './dashboardbottom' component = {DashboardBottom}/>
+            <Route component = {Error}/>
+        </Switch>
+    
+    </>
+    );
 };
 
 export default App;
